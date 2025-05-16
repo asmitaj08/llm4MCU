8c7cdae14aabffeb35cc374500a2dcc619902d9e
asmitaj08 <asmita.jha82@gmail.com>
2025-05-16 13:11:35 -0700

    updated eval_metrics.py

--- huggingFace_exp/hf_codellama_7b_exp/eval_metrics.py
+++ huggingFace_exp/hf_codellama_7b_exp/eval_metrics.py
@@ -82,7 +82,8 @@ def compute_bertscore_batch(predictions, ground_truths, batch_size=64, device='c
     for i in range(0, len(predictions), batch_size):
         batch_preds = predictions[i:i+batch_size]
         batch_gts = ground_truths[i:i+batch_size]
-        _, _, F1 = bertscore(batch_preds, batch_gts, lang='en', device=device)
+        # _, _, F1 = bertscore(batch_preds, batch_gts, lang='en', device=device)
+        _, _, F1 = bertscore(predictions, ground_truths, model_type="BAAI/bge-large-en-v1.5", device='cuda') #"BAAI/bge-large-en-v1.5" used during embedding
         all_scores.extend(F1.tolist())
     return all_scores
 
